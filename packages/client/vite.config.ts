d4e8ad4ff6e089f8db465a085061658427efc27c
Nate Stringham <nstringham@wccnet.edu>
2021-02-09 03:32:13 -0500

    build(client): :heavy_plus_sign: add bulm ui to vue
    folled procedure in https://next-material.balmjs.com/#/guide/quickstart

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  alias: {
+    vue: 'vue/dist/vue.esm-bundler.js',
+    // balm-ui@9.1.0+
+    'balm-ui': 'balm-ui/dist/balm-ui.esm.js',
+    'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
+    'balm-ui-css': 'balm-ui/dist/balm-ui.css',
+  },
+  optimizeDeps: {
+    // Remove useless warning for `balm-ui`
+    exclude: ['balm-ui'],
+  },
 });
